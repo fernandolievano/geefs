bb1df61e25966a2df159b8cf62fbf333bfa34d03
fernandolievano <fernandolievan@gmail.com>
2024-10-15 02:57:48 -0300

    feat: metadata

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <main>
+    <h1>Geefs</h1>
+    <h4>Rebuilding...</h4>
+  </main>
 </template>
+
+<script setup>
+useHead({
+  title: 'Geefs',
+  meta: [
+    { name: 'description', content: ' A simple project that displays GIFs, built by fernandolievano to practice and enhance his Vue.js skills.' },
+  ]
+})
+</script>
