1f02f7d5a269ae9903f7f8566a6d43a754cc1fc4
fernandolievano <fernandolievan@gmail.com>
2024-10-20 06:25:49 -0300

    logo and favicon added

--- app.vue
+++ app.vue
@@ -1,15 +1,20 @@
 <template>
   <main>
-    <h1>Geefs</h1>
+    <h1>Geefs | Gifs search tool</h1>
     <h4>Rebuilding...</h4>
   </main>
 </template>
 
 <script setup>
 useHead({
-  title: 'Geefs',
+  title: 'Geefs | Gifs search tool',
   meta: [
     { name: 'description', content: ' A simple project that displays GIFs, built by fernandolievano to practice and enhance his Vue.js skills.' },
+  ],
+  link: [
+    {
+      rel: 'icon', type: 'image/x-icon', href: '/geef.ico'
+    }
   ]
 })
 </script>
