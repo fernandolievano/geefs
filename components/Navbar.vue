7951856906712b04f392fd307243f8858a2d523b
fernandolievano <fernandolievan@gmail.com>
2025-01-06 09:47:22 -0300

    style: update Navbar styles for improved visibility and animation effects

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar-animation fixed top-0 left-0 bg-transparent backdrop-blur-xl w-full h-[80px]">
+  <nav class="navbar-animation fixed top-0 left-0 w-full h-[80px] z-50 bg-black">
     <div class="flex items-center justify-between h-full px-4 py-4">
       <nuxt-link to="/">
         <img class="w-12 h-12" src="/geef.svg" alt="Logo">
@@ -9,7 +9,7 @@
         <input
           type="text"
           autocomplete="search"
-          class="px-2 py-3 text-sm bg-transparent border rounded rounded-tr-none rounded-br-none border-primary text-primary placeholder:text-primary"
+          class="px-2 py-3 text-sm bg-black border rounded rounded-tr-none rounded-br-none border-primary text-primary placeholder:text-primary"
           placeholder="Find your gifs via Tenor"
         >
         <button class="px-4 py-3 text-sm transition-colors border border-l-0 rounded-tr rounded-br border-primary text-primary hover:bg-secondary hover:text-white">
@@ -30,83 +30,83 @@
 
 @keyframes navbar-border {
   5% {
-    border-bottom: solid 0.1rem #3300cc;
+    box-shadow: 0 0 12px 1px #3300cc;
   }
 
   10% {
-    border-bottom: solid 0.1rem #33ff00;
+    box-shadow: 0 0 12px 1px #33ff00;
   }
 
   15% {
-    border-bottom: solid 0.1rem #4f99be;
+    box-shadow: 0 0 12px 1px #4f99be;
   }
 
   20% {
-    border-bottom: solid 0.1rem #663399;
+    box-shadow: 0 0 12px 1px #663399;
   }
 
   25% {
-    border-bottom: solid 0.1rem #cc6699;
+    box-shadow: 0 0 12px 1px #cc6699;
   }
 
   30% {
-    border-bottom: solid 0.1rem #ccff66;
+    box-shadow: 0 0 12px 1px #ccff66;
   }
 
   35% {
-    border-bottom: solid 0.1rem #cc0099;
+    box-shadow: 0 0 12px 1px #cc0099;
   }
 
   40% {
-    border-bottom: solid 0.1rem #3300cc;
+    box-shadow: 0 0 12px 1px #3300cc;
   }
 
   45% {
-    border-bottom: solid 0.1rem #ffcc33;
+    box-shadow: 0 0 12px 1px #ffcc33;
   }
 
   50% {
-    border-bottom: solid 0.1rem #ccff33;
+    box-shadow: 0 0 12px 1px #ccff33;
   }
 
   55% {
-    border-bottom: solid 0.1rem #99ffff;
+    box-shadow: 0 0 12px 1px #99ffff;
   }
 
   60% {
-    border-bottom: solid 0.1rem #ff3333;
+    box-shadow: 0 0 12px 1px #ff3333;
   }
 
   65% {
-    border-bottom: solid 0.1rem #990066;
+    box-shadow: 0 0 12px 1px #990066;
   }
 
   70% {
-    border-bottom: solid 0.1rem #3333cc;
+    box-shadow: 0 0 12px 1px #3333cc;
   }
 
   75% {
-    border-bottom: solid 0.1rem #cc99cc;
+    box-shadow: 0 0 12px 1px #cc99cc;
   }
 
   80% {
-    border-bottom: solid 0.1rem #669900;
+    box-shadow: 0 0 12px 1px #669900;
   }
 
   85% {
-    border-bottom: solid 0.1rem #66ff00;
+    box-shadow: 0 0 12px 1px #66ff00;
   }
 
   90% {
-    border-bottom: solid 0.1rem #6699ff;
+    box-shadow: 0 0 12px 1px #6699ff;
   }
 
   95% {
-    border-bottom: solid 0.1rem #6600ff;
+    box-shadow: 0 0 12px 1px #6600ff;
   }
 
   100% {
-    border-bottom: solid 0.1rem #cc6600;
+    box-shadow: 0 0 12px 1px #cc6600;
   }
 }
 </style>
